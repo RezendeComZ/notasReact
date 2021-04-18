a71edcc76f5cbe115713ddf83a59fd2097f21905
Gabriel Rezende <g@brielrezen.de>
2021-04-18 16:46:12 -0300

    edit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import PagNotas from './PagNotas';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PagNotas />
   </React.StrictMode>,
   document.getElementById('root')
 );
