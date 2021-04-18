6c24c32a9cf70557d34c29a1921dba27464b26cd
Gabriel Rezende <g@brielrezen.de>
2021-04-18 16:41:51 -0300

    página de notas

---
+++ src/PagNotas.js
@@ -0,0 +1,15 @@
+import Settings from "./settings";
+import "./stylesheets/style.css";
+
+function App() {
+  return (
+    <div className="App">
+    <div className="content">
+    <Settings/>
+      <h1>Notas</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
