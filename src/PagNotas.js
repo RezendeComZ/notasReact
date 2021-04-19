c6e1cdc78eca64b2ac2ee34909187d05d0a6c1b4
Gabriel Rezende <g@brielrezen.de>
2021-04-18 22:11:07 -0300

    notas funcionando sem API

--- src/PagNotas.js
+++ src/PagNotas.js
@@ -1,15 +1,34 @@
 import Settings from "./settings";
 import "./stylesheets/style.css";
+import { useState } from "react"
 
-function App() {
+function PagNotas() {
+  // const titulo = "Notas"
+
+  const [notas, setnotas] = useState([
+    {title: 'Uma nota', body: "a primeira nota", nota_id: 1, pin: false},
+    {title: 'Outra nota', body: "a segunda", nota_id: 2, pin: false},
+    {title: 'Uma fixa nota', body: "a terceira", nota_id: 3, pin: false},
+  ])
   return (
     <div className="App">
-    <div className="content">
     <Settings/>
-      <h1>Notas</h1>
+    <div className="notas">
+      {notas.map( (nota) => 
+        (
+          <div className={"postit pin" + nota.pin} id={nota.nota_id}>
+            <div className="headPost">
+              {nota.title}
+            </div>
+            <div class="bodyPost">
+              {nota.body}
+            </div>
+          </div>
+        )
+      )}
     </div>
     </div>
   );
 }
 
-export default App;
+export default PagNotas;
